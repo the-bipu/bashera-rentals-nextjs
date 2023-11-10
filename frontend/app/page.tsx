40ec21e9454a6b1313ce6392795f7e6e78a94e4f
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-10 21:40:11 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 
-export default function Home() {
+import Home from '@/components/Home'
+
+export default function Page() {
   return (
     <main>
-      
+      <Home />
     </main>
   )
 }
