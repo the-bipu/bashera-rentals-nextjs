dd16283263640e022cb2448579b7b7a89fc8562a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-14 22:33:34 +0530

    Create Home.tsx

---
+++ frontend/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-white'>
+        <div id='home-head' className=''>
+          
+        </div>
+    </div>
+  )
+}
