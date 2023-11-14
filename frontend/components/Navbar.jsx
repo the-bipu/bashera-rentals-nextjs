9b23d1b331409a167a18ed3dbe437f715dcd8c28
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-14 22:02:24 +0530

    Update Navbar.jsx

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 
 import { FaBars } from "react-icons/fa"
+import { IoHome } from "react-icons/io5";
 
 const Navbar = () => {
 
@@ -16,10 +17,13 @@ const Navbar = () => {
     <div className={`sticky top-0 z-40 w-full backdrop-blur h-[73px] flex items-center transition-colors duration-500 lg:z-50 border-b border-slate-900/10 bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent ${isActive ? 'active' : ''}`}>
 
       <div className='w-[100vw] flex items-center justify-between lg:px-[80px] px-[30px]'>
-            <a href="/" className='text-[25px] font-bold text-white'>Bashera</a>
+            <div className='flex flex-row items-center justify-center gap-2 text-[25px]'>
+              <IoHome className='mb-1' /> 
+              <a href="/" className='text-[25px] font-bold text-black'> Bashera</a>
+            </div>
 
             <div id="menu-btn" onClick={toggleNavbar} className="text-[25px] cursor-pointer text-white lg:hidden block">
-              <FaBars className='w-[2rem] h-[2rem] text-white drop-shadow-lg' />
+              <FaBars id='bars-nav' className='w-[2rem] h-[2rem] text-black drop-shadow-lg' />
                 <div className="secNav">
                   <Link href="/">Home</Link>
                   <Link href="/Services">Services</Link>
