a9d20ecdfef79f3f70fd83a9b401ea82a416bd22
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-15 16:57:01 +0530

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import '../app/globals.css'
 
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
