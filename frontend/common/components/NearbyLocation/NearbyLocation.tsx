331d8922f324e5205a7761a04f20c49f6d482b55
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-31 23:06:47 +0530

    Create NearbyLocation.tsx

---
+++ frontend/common/components/NearbyLocation/NearbyLocation.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const NearbyLocation = () => {
+    return (
+        <div className='flex flex-col w-full h-auto bg-[#e6e9f0]'>
+            <div className='text-center p-4 text-[1.4rem]'>Your Nearby Locations</div>
+            <div className='md:px-[5rem] px-[2rem] pt-0 pb-16 flex flex-wrap flex-row items-center justify-center gap-4'>
+
+                {/* First Card */}
+                <div className='bg-[#fff] flex flex-col items-start w-[320px]'>
+                    <div>
+                        <img src="/home-11.jpg" alt="Home Image" className='' />
+                    </div>
+                    <div className='p-4'>
+                        <div>Name</div>
+                        <div>Location</div>
+                    </div>
+                </div>
+
+                {/* Second Card */}
+                <div className='bg-[#fff] flex flex-col items-start w-[320px]'>
+                    <div>
+                        <img src="/home-11.jpg" alt="Home Image" className='' />
+                    </div>
+                    <div className='p-4'>
+                        <div>Name</div>
+                        <div>Location</div>
+                    </div>
+                </div>
+
+                {/* Third Card */}
+                <div className='bg-[#fff] flex flex-col items-start w-[320px]'>
+                    <div>
+                        <img src="/home-11.jpg" alt="Home Image" className='' />
+                    </div>
+                    <div className='p-4'>
+                        <div>Name</div>
+                        <div>Location</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NearbyLocation
