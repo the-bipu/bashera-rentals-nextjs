0b9042c9157b3b4099f1317fe1b630e108a500f9
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-02 11:57:56 +0530

    fix navbar

--- frontend/common/components/Navbar/Navbar.jsx
+++ frontend/common/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './Navbar.scss';
 
 import { FaBars } from "react-icons/fa"
 import { IoHome } from "react-icons/io5";
@@ -8,38 +9,60 @@ import { IoHome } from "react-icons/io5";
 const Navbar = () => {
 
   const [isActive, setIsActive] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const toggleNavbar = () => {
     setIsActive(!isActive);
   };
-  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const offset = 50;
+
+      console.log('Scroll Position:', scrollPosition);
+
+      if (scrollPosition > offset) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={`fixed top-0 z-40 w-full backdrop-blur h-[90px] flex items-center transition-colors duration-500 lg:z-50 border-b border-slate-900/10 supports-backdrop-blur:bg-white/60 bg-transparent ${isActive ? 'active' : ''}`}>
+    <div className={`fixed top-0 z-40 w-full h-auto flex items-center duration-500 ${isActive ? 'active' : ''} ${isScrolled ? 'bg-white' : 'bg-transparent'}`}>
 
-      <div className='w-[100vw] flex items-center justify-between lg:px-[80px] px-[30px]'>
-            <div className='flex flex-row items-center justify-center gap-2 text-[25px]'>
-              <IoHome className='mb-1' /> 
-              <a href="/" className='text-[25px] font-bold text-white'> Bashera</a>
-            </div>
+      <div className={`w-[100vw] h-full flex items-center justify-between text-left lg:px-[80px] px-[20px] transition-all ${isScrolled ? 'py-[15px]' : 'py-[30px]'}`}>
+        <div className='flex flex-row items-center justify-center gap-2 text-[22px]'>
+          <IoHome className={`mb-1 ${isScrolled ? 'text-black' : 'text-white'}`} />
+          <a href="/" className={`text-[22px] font-bold ${isScrolled ? 'text-black' : 'text-white'}`}> Bashera</a>
+        </div>
 
-            <div id="menu-btn" onClick={toggleNavbar} className="text-[25px] cursor-pointer text-white lg:hidden block">
-              <FaBars id='bars-nav' className='w-[2rem] h-[2rem] text-white drop-shadow-lg' />
-                <div className="secNav text-white">
-                  <Link href="/" className='text-white'>Home</Link>
-                  <Link href="/Services" className='text-white'>Services</Link>
-                  <Link href="/About" className='text-white'>About</Link>
-                  <Link href="/Contact" className='text-white'>Contact</Link>
-                  <Link href="/Login" className='text-white'>Login</Link>
-                </div>
-            </div>
+        <div id="menu-btn" onClick={toggleNavbar} className="text-[25px] cursor-pointer text-white lg:hidden block">
+          <FaBars id='bars-nav' className='w-[2rem] h-[2rem] text-white drop-shadow-lg' />
+          <div className="secNav text-white">
+            <Link href="/">Home</Link>
+            <Link href="/Services">Services</Link>
+            <Link href="/About">About</Link>
+            <Link href="/Contact">Contact</Link>
+            <Link href="/Login">Login</Link>
+          </div>
+        </div>
 
-            <nav id='main-nav' className={`lg:flex hidden flex-row gap-[2rem] text-[20px]`}>
-                <Link href="/">Home</Link>
-                <Link href="/Services">Services</Link>
-                <Link href="/About">About</Link>
-                <Link href="/Contact">Contact</Link>
-                <Link href="/Login">Login</Link>
-            </nav>
+        <nav id='main-nav' className={`lg:flex hidden flex-row justify-center items-center gap-[2rem] text-base ${isScrolled ? 'text-black' : 'text-white'}`}>
+          <Link href="/">Home</Link>
+          <Link href="/Services">Services</Link>
+          <Link href="/About">About</Link>
+          <Link href="/Contact">Contact</Link>
+          <button href="/Login" className='py-3 px-8 bg-blue-700 rounded-lg'>Login</button>
+        </nav>
 
       </div>
 
