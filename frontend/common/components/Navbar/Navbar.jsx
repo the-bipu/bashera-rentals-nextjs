8000834a942d20c130b79699dd788bf7200bf458
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-02-05 23:57:54 +0530

    fix nav

--- frontend/common/components/Navbar/Navbar.jsx
+++ frontend/common/components/Navbar/Navbar.jsx
@@ -37,36 +37,55 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className={`fixed top-0 z-40 w-full h-auto flex items-center duration-500 border-b-[1px] border-[#cbcbcb9b] ${isActive ? 'active' : ''} ${isScrolled ? 'bg-white' : 'bg-transparent'}`}>
+    <>
+      <div className={`absolute top-0 z-40 w-full h-auto flex items-center duration-500 border-b-[1px] border-[#cbcbcb79] ${isActive ? 'active' : ''} ${isScrolled ? 'bg-white' : 'bg-transparent'}`}>
 
-      <div className={`w-[100vw] h-full flex items-center justify-between text-left lg:px-[80px] px-[20px] transition-all ${isScrolled ? 'py-[15px]' : 'py-[30px]'}`}>
-        <div className='flex flex-row items-center justify-center gap-2 text-[22px]'>
-          <IoHome className={`mb-1 ${isScrolled ? 'text-black' : 'text-white'}`} />
-          <a href="/" className={`text-[22px] font-bold ${isScrolled ? 'text-black' : 'text-white'}`}> Bashera</a>
-        </div>
+        <div className={`w-[100vw] h-full flex items-center justify-between text-left lg:px-20 px-5 transition-all py-4`}>
+          <div className='flex flex-row items-center justify-center gap-2 text-[22px]'>
+            <IoHome className={`mb-1 ${isScrolled ? 'text-black' : 'text-white'}`} />
+            <a href="/" className={`text-[22px] font-bold ${isScrolled ? 'text-black' : 'text-white'}`}> Bashera</a>
+          </div>
 
-        <div id="menu-btn" onClick={toggleNavbar} className="text-[25px] cursor-pointer text-white lg:hidden block">
-          <FaBars id='bars-nav' className='w-[2rem] h-[2rem] text-white drop-shadow-lg' />
-          <div className="secNav text-white">
+          <div id="menu-btn" onClick={toggleNavbar} className="text-[25px] cursor-pointer text-white lg:hidden block">
+            <FaBars id='bars-nav' className='w-[2rem] h-[2rem] text-white drop-shadow-lg' />
+            <div className="secNav text-white">
+              <Link href="/">Home</Link>
+              <Link href="/Services">Services</Link>
+              <Link href="/About">About</Link>
+              <Link href="/Contact">Contact</Link>
+              <Link href="/Login">Login</Link>
+            </div>
+          </div>
+
+          <nav id='main-nav' className={`lg:flex hidden flex-row justify-center items-center gap-8 text-base font-light ${isScrolled ? 'text-black' : 'text-white'}`}>
             <Link href="/">Home</Link>
             <Link href="/Services">Services</Link>
             <Link href="/About">About</Link>
             <Link href="/Contact">Contact</Link>
-            <Link href="/Login">Login</Link>
-          </div>
+            <button href="/Login" className='py-3 px-8 bg-[#08184b] rounded-sm'>Login</button>
+          </nav>
         </div>
-
-        <nav id='main-nav' className={`lg:flex hidden flex-row justify-center items-center gap-[2rem] text-base ${isScrolled ? 'text-black' : 'text-white'}`}>
-          <Link href="/">Home</Link>
-          <Link href="/Services">Services</Link>
-          <Link href="/About">About</Link>
-          <Link href="/Contact">Contact</Link>
-          <button href="/Login" className='py-3 px-8 bg-blue-700 rounded-lg'>Login</button>
-        </nav>
 
       </div>
 
-    </div>
+      {isScrolled && <div className={`fixed top-0 z-40 w-full h-auto flex items-center duration-500 border-b-[1px] border-[#cbcbcb79] ${isActive ? 'active' : ''} bg-white`}>
+
+        <div className={`w-[100vw] h-full flex items-center justify-between text-left lg:px-20 px-5 transition-all py-6`}>
+          <div className='flex flex-row items-center justify-center gap-2 text-xl'>
+            <IoHome className={`mb-1 text-black`} />
+            <a href="/" className={`text-xl font-bold text-black`}> Bashera</a>
+          </div>
+
+          <nav className={`fixed lg:flex hidden flex-row justify-center items-center w-full h-auto gap-8 text-base font-light text-black bg-white`}>
+            <Link href="/">Home</Link>
+            <Link href="/Services">Services</Link>
+            <Link href="/About">About</Link>
+            <Link href="/Contact">Contact</Link>
+            <button href="/Login" className={`py-3 px-8 bg-[#08184b] text-white border-[1px] border-[#08184b] rounded-sm`}>Login</button>
+          </nav>
+        </div>
+      </div>}
+    </>
   )
 }
 
