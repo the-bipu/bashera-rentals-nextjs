9db10912f4ca5c75b484fac3ff42f162e8c2cfd4
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-02-06 00:20:38 +0530

    Update Footer.jsx

--- frontend/common/components/Footer/Footer.jsx
+++ frontend/common/components/Footer/Footer.jsx
@@ -14,16 +14,11 @@ const Footer = () => {
   
   return (
     <div id='footer-main' className="md:w-full w-[90%] h-auto text-black md:px-[5rem] px-0 py-[2rem]">
-        <div className='h-auto flex items-center justify-center'>
-            <div id='footer-head' className='w-[300px] h-[60px] bg-white font-semibold text-[20px] flex items-center justify-center mt-[-60px] mb-8 rounded-2xl'>
-                🍃Footer🍃
-            </div>
-        </div>
         <div className='flex md:flex-row flex-col items-start justify-around flex-wrap md:p-0 p-6 md:gap-0 gap-6'>
             <div className='md:w-[35%] w-auto flex flex-col gap-3'>
                 <div className='flex flex-row items-center gap-1 text-[22px]'>
                     {/* <IoHome className='mb-1' />  */}
-                    <a href="/" className='text-[25px] font-bold text-black'>🏠 Bashera</a>
+                    <a href="/" className='text-[25px] font-bold text-black'>Bashera.Rentals</a>
                 </div>
                 {/* <div>Discover, Connect, Rent - Your Perfect Home Awaits!</div> */}
                 <div className='text-[14px]'>
@@ -38,30 +33,30 @@ const Footer = () => {
                 </div>
             </div>
             <div className='flex flex-col gap-2'>
-                <div className='text-[#bfbfbf] font-normal'>Quick Links</div>
+                <div className='text-[#bfbfbf] font-normal'>Navigations</div>
                 <div className='flex flex-col gap-1'>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
+                    <a href='' className='font-normal text-[#000]'>About Us</a>
+                    <a href='' className='font-normal text-[#000]'>FAQS</a>
+                    <a href='' className='font-normal text-[#000]'>Contact</a>
+                    <a href='' className='font-normal text-[#000]'>Blog</a>
                 </div>
             </div>
             <div className='flex flex-col gap-2'>
-                <div className='text-[#bfbfbf] font-normal'>Quick Links</div>
+                <div className='text-[#bfbfbf] font-normal'>The Hightlights</div>
                 <div className='flex flex-col gap-1'>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
+                    <a href='' className='font-normal text-[#000]'>Apartments</a>
+                    <a href='' className='font-normal text-[#000]'>My Houses</a>
+                    <a href='' className='font-normal text-[#000]'>Condos</a>
+                    <a href='' className='font-normal text-[#000]'>Villas</a>
                 </div>
             </div>
             <div className='flex flex-col gap-2'>
-                <div className='text-[#bfbfbf] font-normal'>Quick Links</div>
+                <div className='text-[#bfbfbf] font-normal'>Agent & Agency</div>
                 <div className='flex flex-col gap-1'>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
+                    <a href='' className='font-normal text-[#000]'>Agency List</a>
+                    <a href='' className='font-normal text-[#000]'>Agency Details</a>
+                    <a href='' className='font-normal text-[#000]'>Agents List</a>
+                    <a href='' className='font-normal text-[#000]'>Agents Details</a>
                 </div>
             </div>
         </div>
