5b8ff35d2836213e8c38feba5e7f6e7615a43c99
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-02-07 21:24:21 +0530

    Create ExplorePlaces.tsx

---
+++ frontend/common/components/ExplorePlaces/ExplorePlaces.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExplorePlaces = () => {
+    return (
+        <div className='flex flex-col w-full h-auto bg-[#e6e9f0]'>
+            <div className='text-center p-4 text-xl'>Explore Good Places</div>
+            <div className='text-center p-4 text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo id ducimus est aliquid perferendis.</div>
+        </div>
+    )
+}
+
+export default ExplorePlaces
