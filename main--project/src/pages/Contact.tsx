583005645a96611c4fd0b57b4524bbdf9b64cd6c
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-01-06 23:51:47 +0530

    added new pages
    create template for other pages.

---
+++ main--project/src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js';
+
+const Contact: Component = () => {
+    return (
+        <div>
+            <h2>Contact</h2>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ main--project/src/pages/Services.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js';
+
+const Services: Component = () => {
+    return (
+        <div>
+            <h2>Services</h2>
+        </div>
+    )
+}
+
+export default Services;
