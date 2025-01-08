b9e8a2839dc29f407c6835bb25fa9b48b5a1291d
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-01-08 21:37:45 +0530

    Update Home.tsx
    added new sections

--- main--project/src/pages/Home.tsx
+++ main--project/src/pages/Home.tsx
@@ -4,14 +4,43 @@ import Navbar from '@/components/Navbar';
 
 const Home: Component = () => {
     return (
-        <div class='flex flex-col w-full min-h-screen items-center justify-start'>
+        <div class='flex flex-col w-full min-h-screen items-center justify-start bg-[#F8F8F8]'>
 
             <Navbar />
 
             {/* content here */}
             <div class='flex flex-col w-full px-6 h-full'>
-                <div class='rounded-3xl flex text-center w-full min-h-[36rem] items-center bg-anime relative'>
-                    <div class='absolute text-8xl font-bold text-[#353535ce] bottom-4'>Discover Your Bashera Here!</div>
+
+                <div class='rounded-3xl flex text-center w-full min-h-[36rem] items-start bg-anime relative p-10 mb-16'>
+                    <div class='text-6xl font-bold text-[#fff] top-8'>Discover Your Bashera Here!</div>
+                    <div class='absolute -left-4 bottom-0 w-1/2 h-32 rounded-tr-3xl skew-x-[20deg] bg-[#F8F8F8]'>
+                        <div class='-skew-x-[20deg] w-full h-full flex flex-col gap-4 px-10 py-5 items-start text-[#a0a0a0] font-medium text-base'>
+                            <span>More than 100+ cities we're in</span>
+                            <div class='flex flex-row gap-6 w-full'>
+                                <div class='bg-[#a0a0a0] rounded-full w-1/4 h-12'></div>
+                                <div class='bg-[#a0a0a0] rounded-full w-1/4 h-12'></div>
+                                <div class='bg-[#a0a0a0] rounded-full w-1/4 h-12'></div>
+                                <div class='bg-[#a0a0a0] rounded-full w-1/4 h-12'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='w-full h-auto flex flex-col items-center justify-center mb-16'>
+                    <div class='px-5 py-1 bg-slate-400 font-semibold uppercase rounded-full mb-4'>Why choose Us</div>
+                    <div class='w-1/2 text-center text-4xl font-bold mb-10'>We're providing the best and assuring your safety</div>
+                    <div class='flex flex-row gap-8 w-11/12 mb-8'>
+                        <div class='w-1/2 h-36 bg-white shadow rounded-2xl'></div>
+                        <div class='w-1/2 h-36 bg-white shadow rounded-2xl'></div>
+                    </div>
+                    <div class='flex flex-row gap-8 w-11/12'>
+                        <div class='w-1/2 h-36 bg-white shadow rounded-2xl'></div>
+                        <div class='w-1/2 h-36 bg-white shadow rounded-2xl'></div>
+                    </div>
+                </div>
+
+                <div class='rounded-3xl flex text-center w-full min-h-[36rem] items-start bg-[#192939] relative p-10 mb-16'>
+                    Top Properties Section Here
                 </div>
             </div>
         </div>
